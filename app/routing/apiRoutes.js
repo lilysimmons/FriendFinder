2a911d722b03d08c2392d3b1d2a50b783689b02a
Lily <44213621+lilysimmons@users.noreply.github.com>
2019-02-15 21:38:41 -0500

    newfriend goes in friend array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,36 +12,8 @@ app.get("/api/friends", function(req, res) {
     
 
 app.post("/api/friends", function(req, res) {
-  
-    console.log(newFriend);
-  
-    friends.push(newFriend);
-  
-    res.json(newFriend);
+    friends.push(req.body);
+       res.json(friends);
   });
 }
 
-
-
-
-// $("#submit").on("click", function(event) {
-//     event.preventDefault();
-
-//     var newFriend = {
-//       name: $("#name").val().trim(),
-//       photo: $("#photo").val().trim(),
-//       scores: [
-//         $("#question1").val(),
-//         $("#question2").val(),
-//         $("#question3").val(),
-//         $("#question4").val(),
-//         $("#question5").val(),
-//         $("#question6").val(),
-//         $("#question7").val(),
-//         $("#question8").val(),
-//         $("#question9").val(),
-//         $("#question10").val()
-//     ]
-//     };
-
-// });
