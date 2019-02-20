154088f940337b71a14421ab656d0290584ba454
Lily <44213621+lilysimmons@users.noreply.github.com>
2019-02-19 23:26:50 -0500

    brings back pic/added friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,8 @@
 var friends = 
 [{
     name:"Ahmed",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    photo:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/Man_with_freckles.jpg/440px-Man_with_freckles.jpg",
+    //https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores:[
         5,
         1,
@@ -17,7 +18,8 @@ var friends =
   },
   {
     name:"Bob",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    photo:"https://thoughtcatalog.files.wordpress.com/2016/08/photo-1441622915984-05d13dfb3d8c.jpg?w=2280&h=1520&resize=2280,1520&quality=95&strip=all&crop=1",
+    //https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     scores:[
         4,
         3,
@@ -33,7 +35,8 @@ var friends =
   },
   {
     name:"Jane",
-    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    photo: "https://www.gannett-cdn.com/presto/2018/09/24/PCHH/59a3b3c0-2f08-4eca-86ec-1eca12f14911-Gritty.jpg?width=534&height=712&fit=bounds&auto=webp",
+    // "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg" ,
     scores:[
         1,
         2,
@@ -45,6 +48,57 @@ var friends =
         3,
         1,
         1
+      ]
+  },
+  {
+    name:"Amanda",
+    photo: "https://data.whicdn.com/images/252121316/large.jpg",
+    // "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg" ,
+    scores:[
+        5,
+        2,
+        1,
+        3,
+        4,
+        2,
+        1,
+        1,
+        2,
+        5
+      ]
+  },
+  {
+    name:"Jerry",
+    photo: "https://www.gannett-cdn.com/-mm-/eeff8611dcc0d1a8d9974c3185e004ff300ea12f/c=0-108-3443-4698/local/-/media/2017/06/05/USATODAY/USATODAY/636323000103921490-XXX-Max-Rose-Jerry-Lewis-019.jpg?width=534&height=712&fit=crop",
+    // "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg" ,
+    scores:[
+        1,
+        2,
+        3,
+        4,
+        5,
+        5,
+        4,
+        3,
+        2,
+        1
+      ]
+  },
+  {
+    name:"Quixote",
+    photo: "https://d25rq8gxcq0p71.cloudfront.net/language-guide/1516/handsomeguy.jpg",
+    // "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg" ,
+    scores:[
+        3,
+        4,
+        5,
+        5,
+        3,
+        5,
+        5,
+        2,
+        3,
+        4
       ]
   }
 
